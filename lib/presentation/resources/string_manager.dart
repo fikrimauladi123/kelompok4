a47fbed979f9016ca66b04a42f0cb8b022ef4b9f
fikrimauladi123 <fikrimauladi23@yahoo.co.id>
2022-06-17 16:17:44 +0700

    first commit

---
+++ lib/presentation/resources/string_manager.dart
@@ -0,0 +1,164 @@
+class StringManager {
+  static const String onBoardingTitle1 =
+      'Belanja Kebutuhan Sehari-hari Dengan Mudah';
+  static const String onBoardingTitle2 = 'Pembayaran Aman';
+  static const String onBoardingTitle3 = 'Pengiriman Pintu Cepat';
+  static const String onBoardingSubTitle1 =
+      'Tetapkan lokasi Pengiriman Anda. Pilih pakaian Anda dari berbagai produk yang kami butuhkan.';
+  static const String onBoardingSubTitle2 =
+      'Tetapkan lokasi Pengiriman Anda. Pilih pakaian Anda dari berbagai produk yang kami butuhkan.';
+  static const String onBoardingSubTitle3 =
+      'Tetapkan lokasi Pengiriman Anda. Pilih pakaian Anda dari berbagai produk yang kami butuhkan.';
+  static const String nextButton = 'Next';
+  static const String getStartedButton = 'Get Started';
+  static const String signInButton = 'Sign in';
+  static const String signInSubtitle = 'Sign in  your new account on e-shop';
+  static const String signUpButton = 'Sign up';
+  static const String signUpSubtitle = 'Create your new account on e-shop';
+  static const String notHaveAccountButton = 'Don\'t have an account?';
+  static const String forgetPassword = 'Forgot Password';
+  static const String forgetPasswordTextButton = 'Forgot Password?';
+  static const String forgetPasswordSubtitle =
+      'Enter the e-mail associated with your account';
+  static const String sendEmailToChangePassword = 'Send';
+  static const String rememberMe = 'Remember me';
+  static const String orSignInWith = 'or sign in with';
+  static const String orSignUpWith = 'or sign up with';
+  static const String alreadyHaveAccount = 'Do have an account?';
+  static const String userName = 'Username';
+  static const String email = 'E-mail';
+  static const String phone = 'Phone';
+  static const String password = 'Password';
+  static const String google = 'With Google';
+  static const String facebook = 'With Facebook';
+  static const String apple = 'With Apple';
+  static const String currentPassword = 'Current Password';
+  static const String newPassword = 'New Password';
+  static const String confirmPassword = 'Confirm Password';
+  static const String navigationBarHome = 'Home';
+  static const String navigationBarCategories = 'Categories';
+  static const String navigationBarFavorites = 'Favorites';
+  static const String navigationBarCart = 'Cart';
+  static const String navigationBarProfile = 'Profile';
+  static const String searchProduct = 'Search Product';
+  static const String findYourFavoriteProductsOnEShopCommerce =
+      'Temukan Produk Favorit anda Di E-Shop Commerce';
+  static const String bestOffers = 'Penawaran Terbaik';
+  static const String seeAll = 'see all';
+  static const String popularProduct = 'Produk Populer';
+  static const String trendyProduct = 'Produk Trendi';
+  static const String caesarMensHoodie = 'Caesar Mens Hoodie Bundle burgundy.';
+  static const String hoodies = 'Hoodies';
+  static const String prise = '50';
+  static const String prise2 = '20';
+  static const String discount = '20';
+  static const String actualPrice = '100';
+  static const String casualPants = 'Casual Pants For Women Wide Leg';
+  static const String womenFashion = 'Women\'s Fashion';
+  static const String counter = '5';
+  static const String addToCart = 'Add To Cart';
+  static const String rate = 'Rating';
+  static const String edit = 'Edit';
+  static const String name = 'Kelompok4';
+  static const String emailName = 'E-mail :';
+  static const String emailAddress = 'kelompok4@gmail.com';
+  static const String phoneNumber = 'Phone Number :';
+  static const String myPhoneNumber = '089611471404';
+  static const String address = 'Alamat :';
+  static const String addressContent =
+      'Jl. Pakuan PO.BOX 452 Ciheuleut Kab. Bogor 1613 Jawa Barat';
+  static const String filter = 'Filter';
+  static const String reset = 'Reset';
+  static const String category = 'Category';
+  static const String sizes = 'Sizes';
+  static const String sortBy = 'Sort By';
+  static const String filterPrice = 'Price';
+  static const String filterRate = 'Rating';
+  static const String notification = 'Notification';
+  static const String promoCode = 'Add Your Promo Code';
+  static const String subtotal = 'Subtotal';
+  static const String discountOnOrder = 'Discount On Order';
+  static const String couponDiscount = 'Coupon Discount';
+  static const String deliveryCharges = 'Delivery Charges';
+  static const String subtotalPrice = '150\$';
+  static const String discountOnOrderPercent = '20%';
+  static const String couponDiscountPercent = '50%';
+  static const String deliveryChargesPrice = 'Free';
+  static const String total = 'Total';
+  static const String totalPrice = '70\$';
+  static const String proceedToCheckout = 'Proceed To Checkout';
+  static const String color = 'Color';
+  static const String size = 'Size';
+  static const String notificationTitle = 'E-Shop Brand';
+  static const String notificationContent =
+      'There are many variations of passages'
+      ' of Lorem Ipsum available, but the majority have suffered alteration.';
+  static const String yesterday = 'Yesterday';
+  static const String today = 'Today';
+  static const String sideAddress = 'Menofia , EG';
+  static const String recommended = 'Recommended For You';
+  static const String recent = 'Recent Search';
+  static const String resulTitle = 'Men Blue Denim Jacket';
+  static const String shirt = 'Formal Shirt';
+  static const String shirtDescription =
+      'Black Zipper Removable Cappuccino Pump Jacket For Unisex';
+  static const String description = 'Description';
+  static const String review = 'Review';
+  static const String ask = 'Ask Question';
+
+  static const List<String> categoryFilter = [
+    'women\'s fashion',
+    'Pullovers',
+    'Jackets',
+    'Men\'s Fashion',
+    'Pullovers',
+    'Jackets',
+    'Men\'s Fashion',
+    'women\'s fashion',
+    'Pullovers',
+    'Jackets',
+  ];
+  static const List<String> sortedBy = [
+    'Pullovers',
+    'Jackets',
+    'Men\'s Fashion',
+    'women\'s fashion',
+    'Pullovers',
+    'Jackets',
+    'Pullovers',
+  ];
+  static const List<String> sizeFilter = [
+    'Medium',
+    'L',
+    'Xl',
+    'XXL',
+    'XXXL',
+    'XXXXL',
+  ];
+  static const List<String> cartSizes = [
+    'M',
+    'L',
+    'X',
+    'XL',
+    'XXL',
+  ];
+
+  static const sideMenuItemsTitle = [
+    'Order',
+    'My Details',
+    'Payment Methods',
+    'Delivery Address',
+    'Promo Card',
+    'Notification',
+    'Helps',
+    'Contact Us',
+    'Logout',
+    'Dark Mode',
+  ];
+  static const recentSearch = [
+    'men blue denim  jacket',
+    'men blue jacket',
+    'jacket men hip hop',
+    'pink hoodie t-shirt full',
+  ];
+}
